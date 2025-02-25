a1789ed958055a913e4186a43d9846c8c267d234
Дмитрий Винокуров <dima.vinokurov.07@mail.ru>
2025-02-25 14:55:27 +0400

    Созданно базовое меню

--- CalculatorGit/Program.cs
+++ CalculatorGit/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Выберите действие:");
+        Console.WriteLine("1. Ввести A");
+        Console.WriteLine("2. Ввести B");
+        Console.WriteLine("3. Выполнить операцию '+'");
+        Console.WriteLine("4. Выполнить операцию '-'");
+        Console.WriteLine("5. Выполнить операцию '*'");
+        Console.WriteLine("6. Выполнить операцию '/'");
+    }
+}
